011d29c3f8cf739e28eb2a602f34a2a7d8864843
Isa Oxenaar <isamoxenaar@gmail.com>
2020-06-05 21:39:43 +0200

    Created a numbercontainer component

---
+++ components/NumberContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet, Alert } from "react-native";
+
+const NumberContainer = (props) => {
+  <View>
+    <Text>{props.children}</Text>
+  </View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default NumberContainer;
