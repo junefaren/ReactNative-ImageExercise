170c4ea3c272618c7a3f636954eb76eb60bbfebc
Isa Oxenaar <isamoxenaar@gmail.com>
2020-06-07 21:55:28 +0200

    Set up the skeleton of the GameScreen component

---
+++ screens/GameScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const GameScreen = (props) => {};
+
+const styles = StyleScreen.create({});
+
+export default GameScreen;
