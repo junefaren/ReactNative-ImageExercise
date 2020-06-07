958c39df775bd4c6cb754fdb4c85052376f5dcfb
Isa Oxenaar <isamoxenaar@gmail.com>
2020-06-07 22:14:04 +0200

    Created generateRandomBetween function

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,18 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-const GameScreen = (props) => {};
+const generateRandomBetween = (min, max, exclude) => {};
+
+const GameScreen = (props) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNr = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNr === exclude) {
+    return generateRandomBetween;
+  } else {
+    return rndNr;
+  }
+};
 
 const styles = StyleScreen.create({});
 
